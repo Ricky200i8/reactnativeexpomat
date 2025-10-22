8430536ed913d2f95d1ebf47455043802f487321
ricardoariasgal87 <ricardoariasgal87@gmail.com>
2025-10-22 17:33:51 +0000

    Se instalo Estilos tailwind css - se modifico text field

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
-
+import TextField from "../components/textField"
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    
+     <View className="flex-1">
+      <TextField></TextField>
+     </View>
   );
 }
